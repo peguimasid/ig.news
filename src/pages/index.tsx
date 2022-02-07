188e982ba716cf9c3e658bd95e9ab9922f61fdd3
Guilhermo Masid <guilhermomasid@gmail.com>
2022-02-07 20:08:35 -0300

    add some styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div className="w-full h-screen flex justify-center items-center">
-      <h1 className="text-20">Hello world</h1>
+      <h1 className="text-20 text-red">Hello world</h1>
     </div>
   );
 };
