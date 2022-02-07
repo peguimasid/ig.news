5187ff1d587248696d4c44726b5a26c5738db90a
Guilhermo Masid <guilhermomasid@gmail.com>
2022-02-07 20:09:32 -0300

    change tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     extend: {},
   },
   presets: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     screens: {
       sm: '600px',
