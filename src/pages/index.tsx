6b77e68ac886166e5fdf92eb75f5d38ec1410fed
Guilhermo Masid <guilhermomasid@gmail.com>
2022-02-08 19:01:09 -0300

    add title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
-const Home = () => {
+import Head from 'next/head';
+
+import { FunctionComponent } from 'react';
+
+const Home: FunctionComponent = () => {
   return (
-    <div className="bg-black w-full h-screen flex justify-center items-center font-sans">
-      <h1 className="text-20 text-white">Hello world</h1>
-    </div>
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <div className="bg-black w-full h-screen flex justify-center items-center font-sans">
+        <h1 className="text-20 text-white">Hello world</h1>
+      </div>
+    </>
   );
 };
 
