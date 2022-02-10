b2b773a4f870626521da852963bcb55ceaa6df39
Guilhermo Masid <guilhermomasid@gmail.com>
2022-02-10 19:59:47 -0300

    some changes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import clsx from 'clsx';
 
 import { SignInButton } from '../SignInButton';
@@ -12,21 +13,21 @@ export const Header: FunctionComponent = () => {
       <div className="flex items-center max-w-lg h-52 mx-auto px-20">
         <Image src="/images/logo.svg" alt="ig.news" width="110" height="31" />
         <nav className="ml-52 h-52">
-          <a
-            href=""
-            className={clsx(
-              styles.active,
-              'inline-block relative px-5 h-52 leading-13 text-grey-A300 hover:text-white transition'
-            )}
-          >
-            Home
-          </a>
-          <a
-            href=""
-            className="inline-block relative px-5 h-52 leading-13 text-grey-A300 hover:text-white transition ml-20"
-          >
-            Posts
-          </a>
+          <Link href="/">
+            <a
+              className={clsx(
+                styles.active,
+                'inline-block relative px-5 h-52 leading-13 text-grey-A300 hover:text-white transition'
+              )}
+            >
+              Home
+            </a>
+          </Link>
+          <Link href="/posts">
+            <a className="inline-block relative px-5 h-52 leading-13 text-grey-A300 hover:text-white transition ml-20">
+              Posts
+            </a>
+          </Link>
         </nav>
         <div className="ml-auto">
           <SignInButton />
---
+++ src/pages/posts.tsx
@@ -0,0 +1,7 @@
+import { FunctionComponent } from 'react';
+
+const Posts: FunctionComponent = () => {
+  return <h1 className="text-white">posts</h1>;
+};
+
+export default Posts;
