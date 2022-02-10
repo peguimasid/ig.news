789b8cd14ddda390167a10a937741c4ec3ac0aa7
Guilhermo Masid <guilhermomasid@gmail.com>
2022-02-10 19:39:25 -0300

    next auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,17 +1,38 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useMemo } from 'react';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 
+import { useSession, signIn, signOut } from 'next-auth/react';
+
 export const SignInButton: FunctionComponent = () => {
-  const isUserLoggedIn = true;
+  const { data, status } = useSession();
+
+  const loading = useMemo(() => {
+    return status === 'loading';
+  }, [status]);
+
+  const isUserLoggedIn = useMemo(() => {
+    return status === 'authenticated';
+  }, [status]);
+
+  if (loading)
+    return (
+      <button className="h-32 rounded-32 flex bg-grey-A850 justify-center items-center font-semibold px-14 text-white">
+        <FaGithub color="#737380" className="w-12 h-12 mr-10" />
+        Loading...
+      </button>
+    );
 
   return (
-    <button className="h-32 rounded-32 flex bg-grey-A850 justify-center items-center font-semibold px-14 text-white hover:brightness-75 transition">
+    <button
+      onClick={() => (isUserLoggedIn ? signOut() : signIn('github'))}
+      className="h-32 rounded-32 flex bg-grey-A850 justify-center items-center font-semibold px-14 text-white hover:brightness-75 transition"
+    >
       <FaGithub
         color={isUserLoggedIn ? '#04D361' : '#EBA417'}
         className="w-12 h-12 mr-10"
       />
-      {isUserLoggedIn ? 'Guilhermo Masid' : 'Sign in with Github'}
+      {isUserLoggedIn ? data.user.name : 'Sign in with Github'}
       {isUserLoggedIn && <FiX color="#737380" className="w-12 h-12 ml-10" />}
     </button>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,19 @@
 import '../styles/globals.css';
 
 import { FunctionComponent } from 'react';
+import { SessionProvider } from 'next-auth/react';
 import { AppProps } from 'next/app';
 import { Header } from '../components/Header';
 
-const MyApp: FunctionComponent<AppProps> = ({ Component, pageProps }) => {
+const MyApp: FunctionComponent<AppProps> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
   return (
-    <>
+    <SessionProvider session={session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   );
 };
 
