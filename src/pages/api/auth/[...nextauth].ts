efe33a6b8754170096bc5e29348a803b0a70a4ef
Guilhermo Masid <guilhermomasid@gmail.com>
2022-02-12 12:42:43 -0300

    chore: save user in db just if user does not alredy exists

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -15,9 +15,17 @@ export default NextAuth({
   callbacks: {
     async signIn({ user: { email } }) {
       try {
-        await fauna.query(q.Create(q.Collection('users'), { data: { email } }));
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(q.Match(q.Index('user_by_email'), q.Casefold(email)))
+            ),
+            q.Create(q.Collection('users'), { data: { email } }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(email)))
+          )
+        );
         return true;
-      } catch {
+      } catch (err) {
         return false;
       }
     },
