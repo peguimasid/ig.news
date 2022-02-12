eb6d14e7ae07e5ad336c0e47be2da90898ee5298
Guilhermo Masid <guilhermomasid@gmail.com>
2022-02-12 12:22:40 -0300

    chore: save user in db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 
+import { query as q } from 'faunadb';
+import { fauna } from '../../../services/fauna';
+
 export default NextAuth({
   providers: [
     GithubProvider({
@@ -9,4 +12,14 @@ export default NextAuth({
       authorization: 'https://github.com/login/oauth/authorize?scope=read:user',
     }),
   ],
+  callbacks: {
+    async signIn({ user: { email } }) {
+      try {
+        await fauna.query(q.Create(q.Collection('users'), { data: { email } }));
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
